db9734893ba626f47d1ecad332ddce860dafd282
annkamsk <an.kramarska@gmail.com>
2023-05-12 23:43:57 +0200

    Vis for SMVAE

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,18 @@
+from pathlib import Path
 from typing import Dict
 
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+
+DEFAULT_SIZE_GUIDANCE = {
+    "compressedHistograms": 1,
+    "images": 1,
+    "scalars": 0,  # 0 means load all
+    "histograms": 1,
+}
 
 
 class EarlyStopping:
@@ -51,3 +63,68 @@ def log_loss(writer, loss_values: Dict[str, float], iteration: int, train=True) 
             writer.add_scalar(f"PoE_training/{key}", value, iteration)
         else:
             writer.add_scalar(f"PoE_test/{key}", value, iteration)
+
+
+def make_plot(data: pd.DataFrame, columns=[], title="") -> None:
+    names = {
+        "PoE_training/total_loss": "Total loss",
+        "PoE_training/kl": "KL",
+        "total_minus_bi": "Total loss - BI loss",
+        "PoE_training/batch_integration": "BI loss",
+        "PoE_training/rna_mse": "RNA MSE",
+        "PoE_training/msi_mse": "MSI MSE",
+        "PoE_training/private": "Private loss",
+        "PoE_training/shared": "Shared loss",
+        "PoE_training/rna_kl_p": "RNA KL p",
+        "PoE_training/rna_kl_mod": "RNA KL mod",
+        "PoE_training/rna_kl_s": "RNA KL s",
+        "PoE_training/msi_kl_p": "MSI KL p",
+        "PoE_training/msi_kl_mod": "MSI KL mod",
+        "PoE_training/msi_kl_s": "MSI KL s",
+        "PoE_training/recovered_rna_poe": "RNA recovered from PoE",
+        "PoE_training/recovered_msi_poe": "MSI recovered from PoE",
+        "PoE_training/loss_msi_rna": "Translation loss MSI -> RNA",
+        "PoE_training/loss_rna_msi": "Translation loss RNA -> MSI",
+    }
+    n_col = 2
+    n_row = int((len(columns) + 1) / n_col)
+    fig_height = {1: 6, 2: 10, 3: 10, 4: 15}
+    fig, axes = plt.subplots(n_row, n_col, sharex=True, figsize=(20, fig_height[n_row]))
+    fig.suptitle(title, fontsize=15)
+
+    if n_row < 2:
+        for col in range(n_col):
+            loss_type = columns[col]
+            sns.lineplot(ax=axes[col], data=data[loss_type])
+            axes[col].set_title(names[loss_type])
+        return
+
+    for row in range(n_row):
+        for col in range(n_col):
+            if row * n_col + col == len(columns):
+                return
+            loss_type = columns[row * n_col + col]
+            sns.lineplot(ax=axes[row][col], data=data[loss_type])
+            axes[row][col].set_title(names[loss_type])
+            axes[row][col].set_ylabel("")
+
+
+def get_loss_logs(model: str):
+    path = str(Path(__file__).parent.parent / "logs" / "smvaevae_params" / model)
+    event_acc = EventAccumulator(path, DEFAULT_SIZE_GUIDANCE)
+    event_acc.Reload()
+    tags = event_acc.Tags()["scalars"]
+
+    total_loss_events = event_acc.Scalars("PoE_training/total_loss")
+    steps = list(map(lambda x: x.step, total_loss_events))
+    runlog_data = pd.DataFrame({"step": steps})
+
+    for tag in tags:
+        event_list = event_acc.Scalars(tag)
+        runlog_data[tag] = np.asarray(map(lambda x: x.value, event_list))
+
+    if "PoE_training/batch_integration" in tags:
+        runlog_data["total_minus_bi"] = (
+            runlog_data["PoE_training/private"] + runlog_data["PoE_training/shared"]
+        )
+    return runlog_data
